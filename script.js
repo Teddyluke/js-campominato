a8d84a65799ab243140e1becfa5a1e48da188d97
Luca Maglie <luca.maglie2@gmail.com>
2020-07-01 14:48:23 +0200

    refactoring codice

--- script.js
+++ script.js
@@ -2,31 +2,29 @@
 
 var numeriBomba = [];
 var appogginoPerNumeriBomba
-for (var i = 0; i < 16; i++) {
-  appogginoPerNumeriBomba = Math.floor(Math.random() * 100)
-  if (numberController(appogginoPerNumeriBomba, numeriBomba) == true) {
-    i--
-  } else {
-    numeriBomba.push(appogginoPerNumeriBomba)
+var maxBombe = 16;
 
+while (numeriBomba.length < maxBombe ) {
+  appogginoPerNumeriBomba = Math.floor(Math.random() * 100);
+  if (numberController(appogginoPerNumeriBomba, numeriBomba) == false) {
+    numeriBomba.push(appogginoPerNumeriBomba)
   }
 }
-
 console.log(numeriBomba);
-
 // chiedo all'utente di inserire 100 - 16 numeri da inserire uno alla volta sempre compreso tra 1 e 100.  Non si può inserire lo stesso numero più di una volta.
 
 var i = 0;
 var punteggio = 0;
 var numeroUtente
 var controller = true;
+var maxSafeNumber = 100 - maxBombe;
 var numeriUtenteInseriti = []
-while (i < 84 && punteggio < 84 && controller != false) {
+while (i < maxSafeNumber && punteggio < maxSafeNumber && controller != false) {
   numeroUtente = parseInt(prompt("inserisci un numero"));
   if (numberController(numeroUtente, numeriBomba) == true) {
     controller = false;
   } else if (numberController(numeroUtente, numeriUtenteInseriti) == true) {
-   alert("il valore inserito non è valido perchè già presente")
+   alert("il valore inserito non è valido");
    i--
   } else {
     punteggio++
